4b71be4406421f5ea412d0b36c464894eff1ec93
Kbailey28 <k.bailey371@gmail.com>
2021-04-29 21:19:24 -0400

    update

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express ();
 
@@ -8,6 +9,14 @@ app.use(express.json({extended: false}));
 
 app.use('/api/email', require('./routes/api/email'));
 
+if(process.env.NODE_ENV === 'production') {
+
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 8585
 
